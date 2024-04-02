6850fe13c870f26d5f7aef62d9920e8fba34bc81
sinha45 <alfa45leonidas@gmail.com>
2024-04-03 02:17:55 +0600

    passicons added

--- src/components/Manager.jsx
+++ src/components/Manager.jsx
@@ -1,37 +1,58 @@
 import React from "react";
+import { useRef } from "react";
 
 const Manager = () => {
+const ref = useRef()
+  const showPassword = () =>{
+    alert('show password');
+    if(ref.current.src.includes("icons/eyecross.png") ){
+    ref.current.src = "icons/eye.png"}
+    else{
+      ref.current.src = "icons/eyecross.png"
+    }
+  }
+
   return (
     <>
-      <div className="absolute top-0 -z-10 h-full w-full bg-white">
-        <div className="absolute bottom-auto left-auto right-0 top-0 h-[500px] w-[500px] -translate-x-[30%] translate-y-[20%] rounded-full bg-[rgba(173,109,244,0.5)] opacity-50 blur-[80px]"></div>
-      </div>
-      <div className="bg-slate-200 mycontainer">
-        <h1 className="text-4xl font-bold text-center">
-       
-          <span className="text-green-700"> &lt;</span>
+     <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
+      <div className=" mycontainer">
+        <h1 className="text-4xl font-bold text-center text-white">
+          <span > &lt;</span>
           Pass
-          <span className="text-green-700">OP/&gt;</span>
+          <span className="text-green-400">OP/&gt;</span>
         </h1>
-        <p className="text-green-900 text-lg text-center">
+        <p className="text-gray-300
+        
+        
+        text-lg text-center">
           Your Own Password Manager
         </p>
-        
-          <div className="flex flex-col p-4  gap-5 text-black">
-            <input
-              className="rounded-full border border-green-400 w-full  p-4 py-1"
-              type="text"
-            />
-            <div className="flex w-full justify-between gap-5">
-            <input
+
+        <div className="flex flex-col p-4  gap-5 text-black items-center">
+          <input placeholder="Enter website URL"
+            className="rounded-full border border-green-400 w-full  p-4 py-1"
+            type="text"
+          />
+          <div className="flex w-full justify-between gap-5">
+            <input placeholder="Enter UserName"
               className="rounded-full border border-green-400 w-full text-black p-4 py-1"
               type="text"
             />
-            <input
+            <div className="relative">
+            <input placeholder="Enter Password"
               className="rounded-full border border-green-400 w-full text-black p-4 py-1"
               type="text"
             />
+            <span className="absolute right-[8px] top-[4px] cursor-pointer" onClick={showPassword}>
+              <img ref={ref} className="p-1" width={26} src="icons/eye.png" alt="eye" />
+            </span>
+            </div>
           </div>
+          
+          <button className="flex justify-center gap-2 items-center bg-green-600 rounded-full px-9 py-2 w-fit border border-1 border-green-300  hover:bg-green-400"><lord-icon
+            src="https://cdn.lordicon.com/jgnvfzqg.json"
+            trigger="hover"
+          ></lord-icon>Add Password</button>
         </div>
       </div>
     </>
